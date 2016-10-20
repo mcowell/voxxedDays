8a0dc16d38666cb36206870452c270703538a046
mcowell <matthew_cowell@wesleyclover.com>
2016-10-20 11:58:07 +0300

    Initial import

---
+++ src/main/java/com/coweb/DemovoxxedApplication.java
@@ -0,0 +1,12 @@
+package com.coweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovoxxedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovoxxedApplication.class, args);
+	}
+}
---
+++ src/main/java/com/coweb/ExchangeController.java
@@ -0,0 +1,25 @@
+package com.coweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ExchangeController {
+
+	@RequestMapping(value="/{currency}/exchange-rate/", method = RequestMethod.GET)
+	@ResponseBody
+	public String getExchangeRate(@PathVariable("currency") String currency) {
+		if (currency.equals("usd"))
+		{
+			return "1.00";
+		}
+		else
+		{
+			return "not suppported";
+		}
+}
+
+}
